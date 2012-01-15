6849f75c46bbcb2b0550a269316ceed383b519dc
Steven Lovegrove <stevenlovegrove@gmail.com>
2012-01-15 17:48:26 +0000

    Initial commit
    Signed-off-by: Steven Lovegrove <stevenlovegrove@gmail.com>

---
+++ LatexTablet.net/Program.cs
@@ -0,0 +1,79 @@
+﻿/**
+ ** Written by Steven Lovegrove
+ ** http://www.doc.ic.ac.uk/~sl203/
+ */
+ 
+using System;
+using System.Windows.Forms;
+using micautLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+
+namespace LatexTablet.net
+{
+  class Program
+  {
+    static MathInputControl ctrl;
+
+    [STAThread]
+    static void Main(string[] args)
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+
+      ctrl = new MathInputControlClass();
+      ctrl.EnableExtendedButtons(true);
+      ctrl.EnableAutoGrow(true);
+      ctrl.Close += () => Application.ExitThread();
+      ctrl.Insert += new _IMathInputControlEvents_InsertEventHandler(ctrl_Insert);
+      ctrl.SetCaptionText("Copy LaTeX Formular to Clipboard");
+      ctrl.Show();
+      Application.Run();
+    }
+
+    static void ctrl_Insert(string RecoResult)
+    {
+      // Convert To LaTeX expression
+      try
+      {
+        StringReader sr = new StringReader(RecoResult);
+        StringWriter sw = new StringWriter();
+        XPathDocument myXPathDocument = new XPathDocument(sr);
+        XslTransform myXslTransform = new XslTransform();
+        XmlTextWriter writer = new XmlTextWriter(sw);
+        myXslTransform.Load("./mmltex.xsl");
+        myXslTransform.Transform(myXPathDocument, null, writer);
+        writer.Close();
+        string latex = sw.ToString();
+        Clipboard.SetData(DataFormats.Text, (Object)latex);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("Exception: {0}", e.ToString());
+      }
+
+      ctrl.Hide();
+      Application.Exit();
+    }
+  }
+}
+
+
+
+//// Display
+//RecoResult = RecoResult.Replace("<m:", "<");
+//RecoResult = RecoResult.Replace("/m:", "/");
+//try
+//{
+//  StringReader sr = new StringReader(RecoResult);
+//  MathML.MathMLDocument doc = new MathML.MathMLDocument();
+//  doc.Load(sr);
+//  mathml.MathElement = (MathML.MathMLMathElement)doc.DocumentElement;
+//} catch(Exception exp) {
+//  MessageBox.Show(exp.Message + "trace: " + exp.StackTrace);
+//}
---
+++ README.txt
@@ -0,0 +1,18 @@
+README for the TexTablet Program
+
+TexTablet is a really simple (and free!) app that allows
+you to write equations freehand and insert them into a
+LaTeX document.
+
+for more information, see
+http://www.doc.ic.ac.uk/~sl203/
+
+Author:
+Steven Lovegrove
+
+NOTES:
+This software and source may be used for free. The
+package XSLT MathML is a third party library bundled
+with this source package. It is licenced under the MIT
+licence, which you must observe. For details, see the
+README.txt file inside the mxml_tex_xslt directory.
